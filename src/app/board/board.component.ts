143b6215515a178d53d57cbaee2b7dca1d53b5d4
iv-lite <ibekwe.victor@gmail.com>
2019-08-23 10:24:58 +0200

    Created Player class which will be an instance of player; BoardComponent.board property initalized;

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from './player';
+
+const NBR_ROWS = 6;
+const NBR_COLS = 7;
 
 @Component({
   selector: 'l4-board',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  board: Player[][];
 
-  constructor() { }
+  constructor() {
+    this.board = [...new Array(NBR_ROWS)].map( () => [...new Array(NBR_COLS)]);
+  }
 
   ngOnInit() {
+    console.log(this.board);
   }
 
 }
---
+++ src/app/board/player.ts
@@ -0,0 +1,13 @@
+const NBR_PIECES = 21;
+
+export class Player {
+    id: number;
+    color: string;
+    pieces: number;
+
+    constructor(props) {
+        this.id = props.id;
+        this.color = props.color;
+        this.pieces = props.pieces || NBR_PIECES;
+    }
+}
